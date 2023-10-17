487bd7c80d6d393ede99efcf8d287d9a0146c6bf
harwinderiam <harwindercode@gmail.com>
2023-10-17 11:34:42 +0530

    Created PPOST request(name,email only)

--- src/students/controller.js
+++ src/students/controller.js
@@ -23,7 +23,29 @@ const getStudentsById = (req, res) => {
     })
 }
 
+const addStudent = (req,res) => {
+    const{ name, email, age, dob } = req.body;
+    //Check if email exists
+    pool.query('SELECT s FROM students s WHERE s.email = $1', [email] , (error,results) => {
+        if (results.rows.length) {
+            res.send("Email already exists");
+        }
+    })
+}
+const createUser = (request, response) => {
+    const { name, email } = request.body
+  
+    pool.query('INSERT INTO students (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send(`User added with ID: ${results.rows[0].id}`)
+    })
+  }
+
 module.exports = {
     getStudents,
     getStudentsById,
+    addStudent,
+    createUser,
 };
--- src/students/routes.js
+++ src/students/routes.js
@@ -1,6 +1,9 @@
 const { Router } = require("express");
 const router = Router();
 const controller = require('./controller')
+
 router.get("/", controller.getStudents);
 router.get("/:id", controller.getStudentsById);
+router.post("/", controller.createUser);
+
 module.exports = router;
