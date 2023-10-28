173c3212cc0191bbd6a2af8e4a755cccabc7d809
harwinderiam <harwindercode@gmail.com>
2023-10-28 23:25:07 +0530

    all changes for dev

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require('path');
+
 const studentRoutes = require("./src/students/routes");
-
-app.get("/" , (req,res) => {
+console.log("Created server");
+/*app.get("/" , (req,res) => {
     res.send("Hello World");
+});*/
+console.log("Created server");
+//app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', async(req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
-app.use(express.json());
-
-app.use("/students", studentRoutes)
+//app.use("/students", studentRoutes)
 
 app.listen(port, () => {
     console.log(`App is listening at port ${port}`)
--- src/students/routes.js
+++ src/students/routes.js
@@ -5,5 +5,6 @@ const controller = require('./controller')
 router.get("/", controller.getStudents);
 router.get("/:id", controller.getStudentsById);
 router.post("/", controller.createUser);
+router.put("/:id",controller.updateUser)
 
 module.exports = router;
